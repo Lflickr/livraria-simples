ad9a8c976cbe947dba016192cd6612de8594707c
Italo <italo.bello10@gmail.com>
2024-07-28 23:22:21 -0300

    Initial commit

---
+++ src/main/java/me/dio/livraria_simples/LivrariaSimplesApplication.java
@@ -0,0 +1,13 @@
+package me.dio.livraria_simples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/livraria_simples/LivrariaSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.livraria_simples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
